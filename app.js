c078dcf6d224f27ba8c46583be00ee74e683a224
ayushman76 <avenuedesign768@gmail.com>
2023-03-11 23:18:28 +0530

    added changes

--- app.js
+++ app.js
@@ -9,3 +9,56 @@ let versionList = document.getElementById('versionList');
 versionMenu.addEventListener('click',()=>{
     versionList.classList.toggle('active');
 })
+
+// Theme Change 
+let themeCircle = document.getElementsByClassName('themeCircle');
+let body = document.getElementById("body");
+let selected = document.getElementsByClassName('selected');
+
+for(let i = 0; i < themeCircle.length; i++){
+    themeCircle[i].addEventListener('click', function(){
+        for(let i = 0; i < selected.length; i++){
+            switch (this.id) {
+                case "pink":
+                    body.style.backgroundColor = "rgba(231, 0, 231, 0.9)";
+                    body.style.color = "white";
+                    selected[0].style.display = "block";
+                    selected[1].style.display = "none";
+                    selected[2].style.display = "none";
+                    selected[3].style.display = "none";
+                    break;
+    
+                case "grass":
+                    body.style.backgroundColor = "#5FD855B2";
+                    body.style.color = "white";
+                    selected[0].style.display = "none";
+                    selected[1].style.display = "block";
+                    selected[2].style.display = "none";
+                    selected[3].style.display = "none";
+                    break;
+                
+                case "purple":
+                    body.style.backgroundColor = "rgba(0, 0, 209, 0.7)";
+                    body.style.color = "white";
+                    selected[0].style.display = "none";
+                    selected[1].style.display = "none";
+                    selected[2].style.display = "block";
+                    selected[3].style.display = "none";
+                    break;
+    
+                case "night":
+                    body.style.backgroundColor = "#000000"
+                    body.style.color = "white";
+                    selected[0].style.display = "none";
+                    selected[1].style.display = "none";
+                    selected[2].style.display = "none";
+                    selected[3].style.display = "block";
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+      
+    });
+}
